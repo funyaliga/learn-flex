a9214b9878b057eb4af8b6e0ef3f02b694ffb3f8
ruiping.fan <85617901@qq.com>
2017-04-28 16:26:54 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="app">
+    <div class="flex-container">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+        <div class="item">5</div>
+    </div>
+    <div class="radio flex-direction">
+        <div v-for="item in f.flexDirection.options">
+            {{ item }}
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            f: {
+                flexDirection: {
+                    selected: 'row',
+                    options: ['row', 'row-reverse', 'column', 'column-reverse']
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "./assets/main.scss";
+</style>
